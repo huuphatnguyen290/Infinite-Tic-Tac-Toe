0884f48c636f04d6718bef841a9e5360e7a59427
Ryan <--list>
2026-03-22 00:08:18 -0400

    Added Game.h comments

--- Game.h
+++ Game.h
@@ -7,18 +7,27 @@
 
 class Game {
 private:
+    // The game board
     Board board;
+
+    // Two human players
     HumanPlayer player1;
     HumanPlayer player2;
 
 public:
+    // Create 2 players and assign X and O
     Game(const std::string& name1, const std::string& name2)
         : player1(name1, 'X'), player2(name2, 'O') {}
 
     void removeOldestMoveIfNeeded(Player& player) {
+        // Each player can only keep 3 marks on the board
         if (player.getMoves().size() > 3) {
+            // Remove the oldest move from the queue
             Position oldMove = player.getMoves().dequeue();
+
+            // Clear that old mark from the board
             board.clearCell(oldMove.row, oldMove.col);
+
             std::cout << player.getName() << "'s oldest mark disappeared at ("
                       << oldMove.row << ", " << oldMove.col << ").\n";
         }
@@ -28,8 +37,10 @@ public:
         Position move;
 
         while (true) {
+            // Get move from current player
             move = player.getMove();
 
+            // Keep asking if the cell is already used
             if (!board.isEmptyCell(move.row, move.col)) {
                 std::cout << "Cell is already used. Try again.\n";
                 continue;
@@ -38,11 +49,19 @@ public:
             break;
         }
 
+        // Place the mark on the board
         board.placeMark(move.row, move.col, player.getSymbol());
+
+        // Store this move in the player's queue
         player.getMoves().enqueue(move);
+
+        // Remove oldest move if player has more than 3 marks
         removeOldestMoveIfNeeded(player);
+
+        // Show updated board
         board.display();
 
+        // Check if this move creates 3 in a row
         if (board.hasThreeInRow(player.getSymbol(), move.row, move.col)) {
             std::cout << player.getName() << " wins!\n";
             return true;
@@ -61,9 +80,13 @@ public:
     }
 
     void play() {
+        // Show game rules first
         showRules();
+
+        // Show empty board at the beginning
         board.display();
 
+        // Keep playing until one player wins
         while (true) {
             if (takeTurn(player1)) {
                 break;
