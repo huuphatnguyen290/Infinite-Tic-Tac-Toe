59ca5868e0e551e4f4a45cc7065ec63472382712
UnsignedInteger255 <phuctran5219@gmail.com>
2026-03-23 09:05:31 -0400

    Update Player.h
    Added comments

--- Player.h
+++ Player.h
@@ -7,16 +7,19 @@
 
 class Player {
 protected: 
-    std::string name;
-    char symbol;
+    std::string name;   
+    char symbol;        // symbol can either be 'X' or 'O'
     Queue<int> moves;   // store positions 1 to 9
 
 public:
+    // Parameterized constructor
     Player(const std::string& playerName, char playerSymbol)
         : name(playerName), symbol(playerSymbol) {}
 
+    // Destructor
     virtual ~Player() {}
 
+    // Getter functions
     std::string getName() const {
         return name;
     }
@@ -24,11 +27,13 @@ public:
     char getSymbol() const {
         return symbol;
     }
-
+    
     Queue<int>& getMoves() {
         return moves;
     }
 
+    /*  Player getMove() virtual function allow other 
+        derived classes to implement their own getMove() function*/
     virtual int getMove() = 0;
 };
 
@@ -37,13 +42,15 @@ public:
     HumanPlayer(const std::string& playerName, char playerSymbol)
         : Player(playerName, playerSymbol) {}
 
+    // HumanPlayer getMove() function return an int for a position on the board
     int getMove() override {
         std::string input;
-
+        
         while (true) {
             std::cout << name << " (" << symbol << "), enter position (1-9): ";
             std::cin >> input;
-
+            
+            // If input has one digit and is between 1 and 9 inclusive, it is valid
             if (input.length() == 1 && input[0] >= '1' && input[0] <= '9') {
                 return input[0] - '0';
             }
