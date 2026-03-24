a3474b6e3d809fb39ca77804d7fde4eef3ca9472
UnsignedInteger255 <phuctran5219@gmail.com>
2026-03-24 04:53:50 +0000

    Adjusted display() in Game.h for visual clarity

--- Game.h
+++ Game.h
@@ -143,27 +143,27 @@ public:
     ~Game() {}
 
     void display() {
-        std::string line = "--------------";
+        std::string line = "-----------";
         std::cout << "\n";
-        std::cout << board[0][0] << " | " << board[0][1] << " | " << board[0][2]
+        std::cout << " " << board[0][0] << " | " << board[0][1] << " | " << board[0][2]
                 << "\t"
-                << "1 | 2 | 3" << std::endl;
+                << " 1 | 2 | 3" << std::endl;
 
         std::cout << line
                 << "\t"
                 << line << std::endl;
 
-        std::cout << board[1][0] << " | " << board[1][1] << " | " << board[1][2]
+        std::cout << " " << board[1][0] << " | " << board[1][1] << " | " << board[1][2]
                 << "\t"
-                << "4 | 5 | 6" << std::endl;
+                << " 4 | 5 | 6" << std::endl;
 
         std::cout << line                
                 << "\t"
                 << line << std::endl;
 
-        std::cout << board[2][0] << " | " << board[2][1] << " | " << board[2][2]
-                << "        "
-                << "7 | 8 | 9" << std::endl;
+        std::cout << " " << board[2][0] << " | " << board[2][1] << " | " << board[2][2]
+                << "\t"
+                << " 7 | 8 | 9" << std::endl;
 
     }
 
